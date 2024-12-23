3eb8aa6d5282a82d660fbde619895d6f7b0e3613
Irving Emmanuel Canul Sonda <irvingcanul.1994@gmail.com>
2024-12-23 17:17:48 -0600

    Style: basic home template

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,60 +1,171 @@
 import { useState } from 'react';
+import { Box, Button, Grid, Table, Link, VStack, HStack, Heading, Text, createListCollection } from '@chakra-ui/react'
+import {
+    SelectContent,
+    SelectItem,
+    SelectLabel,
+    SelectRoot,
+    SelectTrigger,
+    SelectValueText,
+  } from "../../components/ui/select"
+import './Home.css'
 
 function Home() {
-    const [data] = useState([
-        {
-          empleado: 'Juan Pérez',
-          codigo: 'E001',
-          estado: 'Cerrado',
-          montoPos: 1000,
-          montoFisico: 950,
-          diferencia: -50,
-          fecha: '2024-12-01',
-        },
-        {
-          empleado: 'María López',
-          codigo: 'E002',
-          estado: 'Abierto',
-          montoPos: 2000,
-          montoFisico: 2000,
-          diferencia: 0,
-          fecha: '2024-12-02',
-        },
-    ]);
+    const [subsidiary, setSubsidiary] = useState("");
+    const [location, setLocation] = useState("");
 
     return (
-        <div className="flex">
-            <div className="flex-1 p-6">
-            <h1 className="text-2xl font-bold mb-6">Inicio</h1>
-            <table className="w-full border-collapse border border-gray-300">
-                <thead>
-                <tr className="bg-gray-200">
-                    <th className="border border-gray-300 px-4 py-2">Empleado</th>
-                    <th className="border border-gray-300 px-4 py-2">Código</th>
-                    <th className="border border-gray-300 px-4 py-2">Estado</th>
-                    <th className="border border-gray-300 px-4 py-2">Monto POS</th>
-                    <th className="border border-gray-300 px-4 py-2">Monto Físico</th>
-                    <th className="border border-gray-300 px-4 py-2">Diferencia</th>
-                    <th className="border border-gray-300 px-4 py-2">Fecha</th>
-                </tr>
-                </thead>
-                <tbody>
-                {data.map((row, index) => (
-                    <tr key={index} className="text-center">
-                    <td className="border border-gray-300 px-4 py-2">{row.empleado}</td>
-                    <td className="border border-gray-300 px-4 py-2">{row.codigo}</td>
-                    <td className="border border-gray-300 px-4 py-2">{row.estado}</td>
-                    <td className="border border-gray-300 px-4 py-2">${row.montoPos}</td>
-                    <td className="border border-gray-300 px-4 py-2">${row.montoFisico}</td>
-                    <td className="border border-gray-300 px-4 py-2">${row.diferencia}</td>
-                    <td className="border border-gray-300 px-4 py-2">{row.fecha}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            </div>
-        </div>
+        <Box
+            p={6}
+            boxShadow="xl"
+            borderRadius="lg"
+            bg="white"
+        >
+                 
+            <VStack spacing={4} align="start">
+                <Heading size="md">Selecciona Subsidiaria y Restaurante</Heading>
+                <HStack w="100%">
+                    <Grid
+                        templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }} 
+                        gap={4} 
+                        mb={4}
+                        w="100%"
+                    >
+
+                        <SelectRoot collection={frameworks}
+                            value={subsidiary}
+                            onValueChange={(e) => setSubsidiary(e.value)}
+                        >
+                            <SelectLabel>Selecciona Subsidiaria</SelectLabel>
+                            <SelectTrigger>
+                                <SelectValueText placeholder="Selecciona Subsidiaria" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {frameworks.items.map((movie) => (
+                                    <SelectItem item={movie} key={movie.value}>
+                                        {movie.label}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </SelectRoot>
+
+                        <SelectRoot collection={frameworks} 
+                            value={location}
+                            onValueChange={(e) => setLocation(e.value)}
+                        >
+                            <SelectLabel>Selecciona Centro de consumo</SelectLabel>
+                            <SelectTrigger>
+                                <SelectValueText placeholder="Selecciona Centro de consumo" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {frameworks.items.map((movie) => (
+                                    <SelectItem item={movie} key={movie.value}>
+                                        {movie.label}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </SelectRoot>
+
+                        
+                        <Button className='primary-button'>
+                            Buscar
+                        </Button>
+
+                    </Grid>
+                </HStack>
+            </VStack>
+
+            <Box mb={6}>
+
+                <Grid 
+                    templateColumns={{ base: "1fr", md: "repeat(4, 1fr)" }} 
+                    gap={4} 
+                    mb={4}
+                >
+                    <Text>Subsidiaria: {subsidiary || "No seleccionada"}</Text>
+                    <Text>Restaurante: {location || "No seleccionado"}</Text>
+                    <Text>Fecha: {new Date().toLocaleDateString()}</Text>
+                    <Text>Hora: {new Date().toLocaleTimeString()}</Text>
+                    <Text>Total Ventas: $2000</Text>
+                    <Text>Total Ventas Registradas: $1980</Text>
+                    <Text>Diferencia: $20</Text>
+                    <Text>Estado: Sincronizado</Text>
+                </Grid>
+
+
+
+            </Box>
+
+            <Box>
+                
+                <Grid 
+                    templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }} 
+                    gap={4} 
+                    mb={4}
+                    w="100%"
+                >
+
+                    <Button className='secondary-button'>
+                        Exportar a CSV
+                    </Button>
+
+                    <Button className='primary-button'>
+                        Actualizar Información
+                    </Button>
+                
+                </Grid>
+
+            </Box>
+
+            <Box>
+
+                <Table.ScrollArea rounded="md" borderWidth="1px" maxW="xl">
+                    <Table.Root size="sm" variant="outline" striped>
+                        <Table.Header>
+                            <Table.Row>
+                            <Table.ColumnHeader>Product</Table.ColumnHeader>
+                            <Table.ColumnHeader>Category</Table.ColumnHeader>
+                            <Table.ColumnHeader textAlign="end">Price</Table.ColumnHeader>
+                            </Table.Row>
+                        </Table.Header>
+                        <Table.Body>
+                            {items.map((item) => (
+                            <Table.Row key={item.id}>
+                                <Table.Cell>
+                                    <Link variant="plain" href="#">
+                                        {item.name}
+                                    </Link>
+                                </Table.Cell>
+                                <Table.Cell>{item.category}</Table.Cell>
+                                <Table.Cell textAlign="end">{item.price}</Table.Cell>
+                            </Table.Row>
+                            ))}
+                        </Table.Body>
+                    </Table.Root>
+                </Table.ScrollArea>
+
+            </Box>
+
+        </Box>
+        
     );
 }
+
+const frameworks = createListCollection({
+    items: [
+      { label: "React.js", value: "react" },
+      { label: "Vue.js", value: "vue" },
+      { label: "Angular", value: "angular" },
+      { label: "Svelte", value: "svelte" },
+    ],
+  })
+
+  const items = [
+    { id: 1, name: "Laptop", category: "Electronics", price: 999.99 },
+    { id: 2, name: "Coffee Maker", category: "Home Appliances", price: 49.99 },
+    { id: 3, name: "Desk Chair", category: "Furniture", price: 150.0 },
+    { id: 4, name: "Smartphone", category: "Electronics", price: 799.99 },
+    { id: 5, name: "Headphones", category: "Accessories", price: 199.99 },
+  ]
 
 export default Home;
