a22b3cb22c5a39c14cb35212a12c206780c7f485
Alexis Herrera Saucedo <47467314+AlexisHS458@users.noreply.github.com>
2025-03-03 14:18:39 -0600

    REF: Update import path for Toaster component to use alias

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider,defaultSystem } from "@chakra-ui/react";
 import App from "./App";
-import { Toaster } from "./components/ui/Toaster";
+import { Toaster } from "@components/ui/toaster"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
