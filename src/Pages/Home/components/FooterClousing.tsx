af8cb6ebe729636feca00f00603c206ac9e548ed
Alexis Herrera Saucedo <47467314+AlexisHS458@users.noreply.github.com>
2025-03-18 16:16:14 -0600

    MOD: improve structure and formatting in FooterClousing component

--- src/Pages/Home/components/FooterClousing.tsx
+++ src/Pages/Home/components/FooterClousing.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Box, Flex } from "@chakra-ui/react";
-import { Button } from "@components/ui/button"
+import { Button } from "@components/ui/button";
 import { CurrencyInput } from "@components/NumericInput";
 import { useFooter } from "@context/home/footerClousingContext";
 import type { FooterClousing, TotalModel } from "@models/common.clousing.model";
@@ -18,9 +18,12 @@ import ConfirmDialog from "./ConfirmDialog";
 import Loading from "@components/Loading";
 import { ClousingSave } from "@models/saveClousing.model";
 
-
-function FooterClousing({ clousingType, clousingId, closeDialog, closingConfirmation }: FooterClousing) {
-
+function FooterClousing({
+  clousingType,
+  clousingId,
+  closeDialog,
+  closingConfirmation,
+}: FooterClousing) {
   const [buttonLoading, setButtonLoading] = useState(false);
   const [loading, setloading] = useState(false);
   const [footer, setFooter] = useState<TotalModel | null>(null);
@@ -46,7 +49,6 @@ function FooterClousing({ clousingType, clousingId, closeDialog, closingConfirma
   }, [clousingId, clousingType, getFooterData]);
 
   async function sendClousing() {
-
     setloading(true);
 
     const cash = await getCashData(clousingId);
@@ -60,11 +62,92 @@ function FooterClousing({ clousingType, clousingId, closeDialog, closingConfirma
     const body: ClousingSave = {
       id: clousingId,
       cash: {
-         electronicTips: cash.electronicTips,
-         lines: cash.currencies,
-         tips: cash.tips,
-         total: cash.total,
-
+        electronicTips: cash.electronicTips,
+        lines: cash.currencies,
+        tips: cash.tips ?? 0,
+        total: cash.total ?? { totalPOS: 0, totalPhysical: 0, difference: 0 },
+      },
+      customer: {
+        lines: customer.lines.map((line) => ({
+          amount: line.amount,
+          currency: Number(line.currency),
+          exchangeRate: line.exchangeRate,
+          id: line.id,
+          amountMXN: line.amountMXN,
+          coupons: line.coupons,
+          customers: line.nameClient,
+          valuePAX: line.pax,
+        })),
+        total: customer.total,
+      },
+      intercompany: {
+        total: intercompany.total,
+        lines: intercompany.lines.map((line) => ({
+          amount: line.amount,
+          employeeId: line.employeeId,
+          employeeName: line.employeeName,
+          id: line.id,
+          physicalAmount: line.physicalAmount,
+          subsidiaryId: line.subsidiaryId,
+          subsidiaryname: line.subsidiaryname,
+          ticket: line.ticket,
+        })),
+      },
+      specialCustomer: {
+        total: specialCustomer.total,
+        lines: specialCustomer.lines.map((line) => ({
+          amountMXN: line.ammountMXN,
+          Check: line.check,
+          client: line.client,
+          consumption: line.bill,
+          difference: line.difference,
+          exchangeRate: line.exchangeRate,
+          passengerName: line.passengerName,
+          valueUSD: line.ammountUSD,
+          flight: line.flight,
+          id: line.id,
+          PAX: line.pax,
+          priceCuopon: line.couponPrice,
+          folioCuopon: line.couponFolio,
+          folio: line.couponFolio,
+          folioCuoponUSD: line.couponFolioUSD,
+          value: line.ammount,
+        })),
+      },
+      employee: {
+        total: employee.total,
+        lines: employee.lines.map((line) => ({
+          amount: line.amount,
+          employeeId: Number(line.employeeCode),
+          reasonId: Number(line.reason),
+          ticketId: line.ticket ? Number(line.ticket) : null,
+          id: line.id,
+          //         externalId: line.externalId,
+        })),
+      },
+      prepaid: {
+        total: prepaid.total,
+        lines: prepaid.lines.map((line) => ({
+          client: line.client,
+          difference: line.difference,
+          id: line.id,
+          physical: line.physical,
+          unitPrice: line.unitPrice,
+          totalPOS: line.totalPOS,
+          quantity: line.quantity,
+          supplementsQuantity: line.supplementsQuantity,
+          isEdit: line.isEdit ?? false,
+        })),
+      },
+      tdc:{
+        total: tdc.total,
+        lines: tdc.lines.map((line) => ({
+          bank: line.bank,
+          id: line.id,
+          physical: line.physical,
+          POS: line.POS,
+          voucherAmount: line.voucherAmount,
+        })),
       }
     };
 
@@ -84,11 +167,10 @@ function FooterClousing({ clousingType, clousingId, closeDialog, closingConfirma
         specialCustomer: body.specialCustomer.total.totalPhysical,
         employee: body.specialCustomer.total.totalPhysical,
         prepaid: body.prepaid.total.totalPhysical,
-        intercompany: body.intercompany.total.totalPhysical
+        intercompany: body.intercompany.total.totalPhysical,
       });
 
       closeDialog();
-
     } else {
       console.log("Error al enviar el corte de caja");
       //showToast(ALERTCLOUSING_MODEL.ERROR, response.error);
@@ -100,47 +182,48 @@ function FooterClousing({ clousingType, clousingId, closeDialog, closingConfirma
 
   return (
     <>
-      <Box
-        p={4}
-        mb={2}
-        mt={4}
-        gap="4"
-        flexDir={{ base: "column", md: "row" }}>
-
+      <Box p={4} mb={2} mt={4} gap="4" flexDir={{ base: "column", md: "row" }}>
         <Flex gap="4" flexDir={{ base: "column", md: "row" }}>
-
           <CurrencyInput
             name={"Total POS"}
             value={footer?.totalPOS ?? 0}
-            loading={false}/>
+            loading={false}
+          />
 
           <CurrencyInput
             name={"Total físico"}
             value={footer?.totalPhysical ?? 0}
-            loading={false}/>
+            loading={false}
+          />
 
           <CurrencyInput
             name={"Diferencia"}
             value={footer?.difference ?? 0}
-            loading={false}/>
+            loading={false}
+          />
 
-          <Button loading={loading}
+          <Button
+            loading={loading}
             colorPalette="meraPrimary"
-            onClick={async () => { setButtonLoading(true); }} disabled={closingConfirmation}>
+            onClick={async () => {
+              setButtonLoading(true);
+            }}
+            disabled={closingConfirmation}
+          >
             Confirmar Corte
           </Button>
-
         </Flex>
 
-        <Flex>
-        </Flex>
-
+        <Flex></Flex>
       </Box>
 
-      <ConfirmDialog isOpen={buttonLoading} closeDialog={() => setButtonLoading(false)} sendData={sendClousing} />
+      <ConfirmDialog
+        isOpen={buttonLoading}
+        closeDialog={() => setButtonLoading(false)}
+        sendData={sendClousing}
+      />
 
       {loading && <Loading />}
-
     </>
   );
 }
