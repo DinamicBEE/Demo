d1aa9874d5e3ff006cab5088ded6d16c2931236e
irving.canul@beexponentialgroup.com <irving.canul@beexponentialgroup.com>
2025-05-06 11:14:23 -0600

    update test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
     alias: {
-      '@components': '/src/components',
-      '@services': '/src/services',
-      '@context': '/src/context',
-      '@styles': '/src/styles',
-      '@hooks': '/src/hooks',
-      '@models': '/src/models',
-      "@utils": "/src/utils/*",
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@models': path.resolve(__dirname, './src/models'),
+      "@utils": path.resolve(__dirname, "./src/utils/*"),
     },
   },
 });
