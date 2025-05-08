7aa39aafdef23757fd761864ccb6c2f30439cd07
Alexis Herrera Saucedo <alexis.herrera@beexponentialgroup.com>
2025-05-07 18:38:25 -0600

    bug

--- src/Pages/Home/components/FooterClousing.tsx
+++ src/Pages/Home/components/FooterClousing.tsx
@@ -42,16 +42,14 @@ function FooterClousing({
 
   const { getFooterData } = useFooter();
   const { getCashData, cashRef } = useCashClousing();
-  const { getTDCData,tdcRef } = useTDCContext();
-  const { getCustomerData,customerRef } = useCustomerContext();
+  const { getTDCData, tdcRef } = useTDCContext();
+  const { getCustomerData, customerRef } = useCustomerContext();
   const { getSpecialCustData, specialCustRef } = useSpecialCustContext();
   const { getEmployeetData, setEmployee } = useEmployeeContext();
-  const { getIntercompanyData,setIntercompany } = useIntercompanyContext();
+  const { getIntercompanyData, setIntercompany } = useIntercompanyContext();
   const { getPrepaidData, prepaidRef } = usePrepaidContext();
   const { setDataClousing } = useClousing();
   const { header } = useHeaders();
-
-  
 
   useEffect(() => {
     async function fetchFooterData() {
@@ -211,7 +209,7 @@ function FooterClousing({
     console.log(body);
 
     const response: any = await sendCashClousing(body, isConfirm);
-console.log("response", response);
+    console.log("response", response);
 
     if (response === "response") {
       //TODO: DEvolver para el back
@@ -220,11 +218,9 @@ console.log("response", response);
       //showToast(ALERTCLOUSING_MODEL.SUCCESS, null);
       //se guardan los datos del corte para poder actualiza la tabla principal
       const statuss =
-        header[body.id] && header[body.id].difference !== 0
+        header[body.id] && Number(header[body.id].difference?.toFixed(2)) !== 0
           ? STATUS.WITH_DIFFERENCE
           : STATUS.Close;
-
-
 
       setDataClousing({
         id: body.id,
@@ -236,23 +232,23 @@ console.log("response", response);
         employee: body.specialCustomer.total.totalPhysical,
         prepaid: body.prepaid.total.totalPhysical,
         intercompany: body.intercompany.total.totalPhysical,
-        mxm: body.cash.lines.find(
-          (line) => line.currency === "MXN"
-        )?.totalFisico ?? 0,
-        usd: body.cash.lines.find(
-          (line) => line.currency === "USD"
-        )?.totalFisico ?? 0,
-        eur: body.cash.lines.find(
-          (line) => line.currency === "EUR"
-        )?.totalFisico ?? 0,
-        lib: body.cash.lines.find(
-          (line) => line.currency === "LIB"
-        )?.totalFisico ?? 0,
-        can: body.cash.lines.find(
-          (line) => line.currency === "CAN"
-        )?.totalFisico ?? 0,
+        mxm:
+          body.cash.lines.find((line) => line.currency === "MXN")
+            ?.totalFisico ?? 0,
+        usd:
+          body.cash.lines.find((line) => line.currency === "USD")
+            ?.totalFisico ?? 0,
+        eur:
+          body.cash.lines.find((line) => line.currency === "EUR")
+            ?.totalFisico ?? 0,
+        lib:
+          body.cash.lines.find((line) => line.currency === "LIB")
+            ?.totalFisico ?? 0,
+        can:
+          body.cash.lines.find((line) => line.currency === "CAN")
+            ?.totalFisico ?? 0,
         status: isConfirm === true ? STATUS.Open : statuss,
-        closingConfirmation: !isConfirm
+        closingConfirmation: !isConfirm,
 
         /*   header[body.id] && header[body.id].difference !== 0
             ? STATUS.WITH_DIFFERENCE
@@ -261,7 +257,7 @@ console.log("response", response);
 
       closeDialog();
       if (isConfirm === true) {
-        cashRef.current = {}
+        cashRef.current = {};
         tdcRef.current = {};
         customerRef.current = {};
         specialCustRef.current = {};
@@ -277,12 +273,12 @@ console.log("response", response);
     setButtonLoading(false);
   }
 
-  const handleDialogConfirm = (isConfirm:boolean) => {
-
-    if (header[clousingId]?.difference && header[clousingId]?.difference != 0 &&
+  const handleDialogConfirm = (isConfirm: boolean) => {
+    if (
+      header[clousingId]?.difference &&
+      Number(header[clousingId]?.difference.toFixed(2)) !== 0 &&
       isConfirm === false
     ) {
-
       setOpenDialogDifference(true);
       return;
     }
