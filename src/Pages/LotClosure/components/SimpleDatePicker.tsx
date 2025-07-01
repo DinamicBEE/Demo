d50081cca853cfbee67bae89519b65eba5018cd6
Manuel Poot <juan.poot@beexponentialgroup.com>
2025-07-01 16:09:12 -0600

    Fix fecha simpledatepicker

--- src/Pages/LotClosure/components/SimpleDatePicker.tsx
+++ src/Pages/LotClosure/components/SimpleDatePicker.tsx
@@ -56,7 +56,7 @@ const SimpleDatePicker: React.FC<DatePickerProps> = ({ onDateChange, initialDate
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     
-    return `${day}/${month}/${year}`;
+    return `${year}-${month}-${day}`;
   };
 
   // Efecto para notificar al padre cuando cambia la fecha
