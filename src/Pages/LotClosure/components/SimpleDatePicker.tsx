f89bf595de8c22f2075d35c72c459386101a0f03
Manuel Poot <juan.poot@beexponentialgroup.com>
2025-08-04 14:00:43 -0600

    Repara bug en calendario

--- src/Pages/LotClosure/components/SimpleDatePicker.tsx
+++ src/Pages/LotClosure/components/SimpleDatePicker.tsx
@@ -67,6 +67,7 @@ const SimpleDatePicker: React.FC<DatePickerProps> = ({ onDateChange, initialDate
   return (
     <ReactDatePicker
       selected={selectedDate}
+      popperProps={{strategy: 'fixed'}} 
       onChange={(date: Date | null) => setSelectedDate(date)}
       isClearable={true}
       dateFormat="dd/MM/yyyy"
