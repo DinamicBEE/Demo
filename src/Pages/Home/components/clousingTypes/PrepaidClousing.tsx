f1d760c16a4ceefb1e2032918fc2873bf4c9c3dd
irving.canul@beexponentialgroup.com <irving.canul@beexponentialgroup.com>
2025-12-22 18:02:13 -0600

    Prepago cambio de idcliente en objeto principal

--- src/Pages/Home/components/clousingTypes/PrepaidClousing.tsx
+++ src/Pages/Home/components/clousingTypes/PrepaidClousing.tsx
@@ -157,7 +157,7 @@ function PrepaidClousing({ data, subsidiaryId, cdc }: any) {
         supplementsQuantity: 0,
         unitPrice: couponModel.amount,
         client: couponModel.clientCustom.toLocaleUpperCase(),
-        idClient: 3,
+        idClient: couponModel.clientId || 0,
         physical: couponModel.amount,
         totalPOS: couponModel.amount * 1,
         difference: 0,
