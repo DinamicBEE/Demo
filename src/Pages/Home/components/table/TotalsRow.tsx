303d2b0172a2dc1539a676165121ce43818a80bd
DinamicBEE <irving.canul@beexponentialgroup.com>
2026-05-22 10:55:57 -0600

    Remove commented-out cell for extra totals in TotalsRow component

--- src/Pages/Home/components/table/TotalsRow.tsx
+++ src/Pages/Home/components/table/TotalsRow.tsx
@@ -30,13 +30,6 @@ function TotalsRow(){
           />
         </Table.Cell>
         <Table.Cell />
-        {/* <Table.Cell textAlign="end">
-          <FormatNumber
-            value={totals.extra}
-            style="currency"
-            currency="USD"
-          />
-        </Table.Cell> */}
 
         {currHeader.length > 0 &&
           totals.currencies != undefined &&
